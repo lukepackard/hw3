27f51def8d5c4496c4d22be34d6e0e3e881162f7
Luke Packard <luke.packard@kellogg.northwestern.edu>
2024-02-18 22:47:35 +0000

    Working on the individual pages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,18 @@
 class PlacesController < ApplicationController
 
 def index 
+  @places = Place.all 
+
   render :template => "places/index"
 end 
 
+def show 
+  @places = Place.find_by({"id" => 1}) 
+
 end
+
+# def new 
+# def create 
+# def create 
+
+

a0fb08d4d645c637277481544ed3936d23073672
Luke Packard <luke.packard@kellogg.northwestern.edu>
2024-02-20 05:41:08 +0000

    Need to create a couple places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,18 +1,33 @@
 class PlacesController < ApplicationController
 
 def index 
+  #puts 
+    render :template => "places/index"
   @places = Place.all 
-
-  render :template => "places/index"
+  # render :template => "places/index"
 end 
 
 def show 
-  @places = Place.find_by({"id" => 1}) 
+  @place = Place.find_by({"id" => params["id"]}) 
 
 end
 
-# def new 
-# def create 
-# def create 
+def new
+
+end
+
+def create 
+
+  @place = Place.new
+  @place["title"] = params["title"]
+  @place["description"] = params["description"]
+  @place["posted on"] = params["posted_on"]
+  
+@Place.save
+
+  redirect_to "/places"
+
+end 
+end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-get "/places", :controller => "places", :action => "index"
-
+#get "/places", :controller => "places", :action => "index"
+resources :places
   # Defines the root path route ("/")
-  get "/entries", :controller => "entries", :action => "index"
-
+ #get "/entries", :controller => "entries", :action => "index"
+resources :entries
   # get("/", { :controller => "articles", :action => "index" })
 end

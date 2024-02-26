2e4e2ed8ceb7fceaf6323f174096fdaff7902571
Luke Packard <luke.packard@kellogg.northwestern.edu>
2024-02-26 01:23:18 +0000

    Need to figure out the entry form

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,13 +1,16 @@
 class EntriesController < ApplicationController
 
   def index
-  render :inline => "<h1> Places I've Been </h1>"
+
   end
+
 def new 
   @entry = Entry.new
-  @entry.place_id = params["place_id"]
-  @place = place.find_by ({"id" => params["place_id"] })
-end
+  @entry.place_id = params[:place_id] 
+  @place = Place.find(params[:place_id])
+  end
+  
+  
 
     def create
       @entry = Entry.new(params["entry"])
